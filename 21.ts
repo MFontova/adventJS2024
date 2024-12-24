0c6be87515a7f4ef8b0211dd6c41fb85985b8aae
Marc Fontova <mfontovat@gmail.com>
2024-12-24 19:53:46 +0100

    refactor day 21

--- 21.ts
+++ 21.ts
@@ -1,37 +1,8 @@
-function treeHeight(tree: { value: string; left: any; right: any } | null): number {
-  if(!tree) return 0
-  let deep = 1
-  
-  function recursiveCalc(node: { value: string; left: any; right: any }, deep: number): number {
-    if (node.left == null && node.right == null) {
-      return deep
-    }
-    if(node.left) {
-      return recursiveCalc(node.left, deep + 1)
-    }
-    if(node.right) {
-      return recursiveCalc(node.right, deep + 1)
-    }
+function treeHeight(tree: { value: string; left: any; right: any } | null): number {  
+  if (!tree) {
     return 0
   }
-
-  return recursiveCalc(tree!, deep)
-  
-
-  // let queue: {value: string; left: any; right: any}[] = [tree]
-
-  // while(queue.length > 0) {
-  //   const queueLength = queue.length  
-  //   for (let i = 0; i < queueLength; i++) {
-  //     const node = queue.shift()!;
-
-  //     if (node.left) queue.push(node.left);
-  //     if (node.right) queue.push(node.right);
-  //   }
-
-  //   counter++
-  // }
-  // return counter
+  return Math.max(treeHeight(tree.left), treeHeight(tree.right)) + 1
 }
 
 // Definición del árbol
